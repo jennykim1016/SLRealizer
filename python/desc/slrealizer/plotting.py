1738aa557e735c95bbcd6255272bb2d34357a867
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-04-03 23:09:11 +0900

    boundary for lensing galaxy

--- python/desc/slrealizer/plotting.py
+++ python/desc/slrealizer/plotting.py
@@ -44,10 +44,15 @@ def draw_model(currObs, currLens, convolved=False):
 
     for i in range(4):
         
+        print i
+        print currLens['MAG'][0]
+        print currLens['NIMG']
         #print "In 'draw_model', mag_ratio, quasar_alpha, lens_alpha =", \
         #    mag_ratio, quasar_alpha, lens_alpha
         lens_mag = currLens[filterLens]
-        quasar_mag = currLens['MAG'][0][i]
+        quasar_mag = currLens[filterQuasar] + 2.5*np.log10(currLens['MAG'][0][i])
+        print 'Quasar Mag, Lens Mag'
+        print quasar_mag, lens_mag
         
         mag_ratio = math.pow(2.5, -lens_mag+quasar_mag)
         # assume that lens_alpha is always greater than quasar_alpha
@@ -55,7 +60,8 @@ def draw_model(currObs, currLens, convolved=False):
         quasar_alpha, lens_alpha = determine_alpha(mag_ratio)
         print 'Quasar Alpha, Lens Alpha'
         print quasar_alpha, lens_alpha
-        galaxy_HWHM = 0.0
+        # no convolution by default
+        galaxy_HWHM = currObs[2]
         PSF_HWHM = currObs[2]
         if(convolved):
             lens_FWHM = currLens['REFF']
@@ -65,11 +71,6 @@ def draw_model(currObs, currLens, convolved=False):
             #      The IMAGE positions are in teh OM10 catalog in the
             #      XIMG and YIMG columns.
         # Draw lens galaxy:
-        lens = plt.Circle((lensX, lensY),
-            radius=galaxy_HWHM,
-            alpha=lens_alpha,
-            fc=circleColor, linewidth=3)
-        plt.gca().add_patch(lens)
         print 'Quasar Position X, Y', \
             lensX+sourceX[i], lensY+sourceY[i]
         source = plt.Circle((lensX+sourceX[i], lensY+sourceY[i]),
@@ -81,10 +82,18 @@ def draw_model(currObs, currLens, convolved=False):
         seeing = plt.Circle((-2.5, -2.5),
                             radius=PSF_HWHM,
                             alpha=0.1,
-                            fc='black')
-        plt.legend((source, seeing, lens),
-                ('QSO images', 'PSF', 'Lens galaxy'), fontsize=10)
+                            fc='gray')
         plt.gca().add_patch(seeing)
+        lens = plt.Circle((lensX, lensY),
+                  radius=galaxy_HWHM,
+                  alpha=lens_alpha,
+                  fc='black', linewidth=3)
+        print 'Lens Position X, Y', \
+            lensX, lensY
+        plt.gca().add_patch(lens)
+        plt.legend((source, seeing, lens),
+           ('QSO images', 'PSF', 'Lens galaxy'), fontsize=10)
+
 
     #plotX, plotY = determine_scale(lensX, sourceX, lensY, sourceY)
     # In order to improve function readabilty, actual plotting involving matplotlib is decomposed
