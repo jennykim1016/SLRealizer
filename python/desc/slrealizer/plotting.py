244b63f9a9c61654398c0461d6fc5eb2cbf7423f
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-04-04 00:08:38 +0900

    got rid of print statements

--- python/desc/slrealizer/plotting.py
+++ python/desc/slrealizer/plotting.py
@@ -4,6 +4,7 @@ import pylab
 import matplotlib
 import math
 import numpy as np
+np.set_printoptions(threshold='nan')
 
 def draw_model(currObs, currLens, convolved=False):
     """
@@ -33,6 +34,7 @@ def draw_model(currObs, currLens, convolved=False):
         raise ValueError('Unknown filter name '+currObs[1])
 
     plt.axis('scaled')
+    # some magic scales that make plots easy-to-read
     plt.ylim(-3, 3)
     plt.xlim(-3, 3)
     plt.xlabel('xPosition')
@@ -46,28 +48,25 @@ def draw_model(currObs, currLens, convolved=False):
         #print "In 'draw_model', mag_ratio, quasar_alpha, lens_alpha =", \
         #    mag_ratio, quasar_alpha, lens_alpha
         lens_mag = currLens[filterLens]
-        quasar_mag = currLens[filterQuasar] - 2.5*np.log10(currLens['MAG'][0][i])
-        print 'Quasar Mag, Lens Mag'
-        print quasar_mag, lens_mag
+        #print 'Quasar Magnitude before 2.5nplog10'
+        #print currLens[filterQuasar]
+        #print '2.5nplog10 term'
+        quasar_mag = currLens[filterQuasar]
+        if currLens['MAG'][0][i] is not 0:
+            quasar_mag = currLens[filterQuasar] - 2.5*np.log10(abs(currLens['MAG'][0][i]))
         
         mag_ratio = math.pow(2.5, -lens_mag+quasar_mag)
-        # assume that lens_alpha is always greater than quasar_alpha
-        # lens_alpha is always one(1)
         quasar_alpha, lens_alpha = determine_alpha(mag_ratio)
-        print 'Quasar Alpha, Lens Alpha'
-        print quasar_alpha, lens_alpha
+        #print 'Quasar Alpha, Lens Alpha'
+        #print quasar_alpha, lens_alpha
         # no convolution by default
         galaxy_HWHM = currObs[2]
         PSF_HWHM = currObs[2]
         if(convolved):
             lens_FWHM = currLens['REFF']
             galaxy_HWHM = convolve(PSF_HWHM, lens_FWHM)/scale_factor
-            # Bug: the LENS position is in the CENTER of the field.
-            #      The SOURCE position is UNOBSERVABLE.
-            #      The IMAGE positions are in teh OM10 catalog in the
-            #      XIMG and YIMG columns.
         # Draw lens galaxy:
-        print 'Quasar Position X, Y', \
+        #print 'Quasar Position X, Y', \
             lensX+sourceX[i], lensY+sourceY[i]
         source = plt.Circle((lensX+sourceX[i], lensY+sourceY[i]),
                             radius=PSF_HWHM,
@@ -84,38 +83,18 @@ def draw_model(currObs, currLens, convolved=False):
                   radius=galaxy_HWHM,
                   alpha=lens_alpha,
                   fc=circleColor, hatch = '/')
-        print 'Lens Position X, Y', \
-            lensX, lensY
+        #print 'Lens Position X, Y', \
+        lensX, lensY
         plt.gca().add_patch(lens)
-        plt.legend((source, seeing, lens),
-           ('QSO images', 'PSF', 'Lens galaxy'), fontsize=10)
-
-
-    #plotX, plotY = determine_scale(lensX, sourceX, lensY, sourceY)
-    # In order to improve function readabilty, actual plotting involving matplotlib is decomposed
-    #plot_figure_on_matplotlib(currObs, convolve, sourceX, sourceY, lensX, lensY, plotX, plotY)
-
-
-
-def plot_figure_on_matplotlib(currObs, convolve, sourceX, sourceY, lensX, lensY, plotX, plotY):
-    """
-        Actual plotting involving matplotlib happens here.
-        Based on the filter's wavelength, we chose five different colors to represent the lensed system.
-    """
-    #fig = plt.figure(1)
+        if i is 0:
+            plt.legend((source, seeing, lens),
+                       ('QSO images', 'PSF', 'Lens galaxy'), fontsize=10)
 
 
 def determine_alpha(flux_ratio):
     """
         Given flux ratio, return alpha value for each quasar and lens.
         Brighter object will have alpha = 1, and fainter object will have magnitude = 1/flux_ratio
-    """
-    """
-    Bug: No docstring
-    Bug: Each image should have its own alpha, because the different
-         images have different magnifications (these are in the OM10 "MAG" columns, and need to be applied to each image's sourceX
-         magnitude.) =>? just to make sure, different magnifications but same instrument?
-    Bug: function name should be "determine_alpha" to be PEP8 compliant
     """
     if(flux_ratio>1):
         quasar_alpha = 1/flux_ratio
@@ -124,19 +103,6 @@ def determine_alpha(flux_ratio):
         quasar_alpha = 1
         lens_alpha = flux_ratio
     return quasar_alpha, lens_alpha
-
-"""
-def determine_scale(lensX, sourceX, lensY, sourceY):
-    if (abs(lensX[0]+sourceX))>(abs(sourceX)):
-        plotX = abs(lensX[0]+sourceX)
-    else:
-        plotX = abs(sourceX)
-    if (abs(lensY[0]+sourceY))>(abs(sourceY)):
-        plotY = abs(lensY[0]+sourceY)
-    else:
-        plotY = abs(sourceY)
-    return plotX, plotY
-"""
 
 def convolve(obsFWHM, initialFWHM=0.0):
     """
