22bf4509d0fbd36eb9fe969828312009ff6a39de
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-08-02 17:31:07 -0700

    fixed a bug in the first moment

--- python/desc/slrealizer/null_deblend.py
+++ python/desc/slrealizer/null_deblend.py
@@ -31,8 +31,14 @@ x_min = -5.0
 x_max = 5.0
 y_min = -5.0
 y_max = 5.0
+
+x_min = 0.0
+x_max = 10.0
+y_min = 0.0
+y_max = 10.0
+
 distance = 0.01
-area_multivariate = 10200 #9999.9610319184358
+area_multivariate = 9999.9610319184358
 number_of_rows = int((x_max - x_min)/distance)
 number_of_columns = int((y_max - y_min)/distance)
 #=======================================================================
@@ -53,7 +59,7 @@ def null_deblend(image2):
     zeroth_moment = moment_matrix[0][0]
     first_moment_x = x_min + (moment_matrix[0][1] / zeroth_moment) * distance
     first_moment_y = y_min + (moment_matrix[1][0] / zeroth_moment) * distance
-    moment_matrix = skimage.measure.moments_central(image2, moment_matrix[1][0]/zeroth_moment, moment_matrix[0][1]/zeroth_moment)
+    moment_matrix = skimage.measure.moments_central(image2, moment_matrix[0][1]/zeroth_moment, moment_matrix[1][0]/zeroth_moment)
     covariance_matrix = [[moment_matrix[0][2], moment_matrix[1][1]], [moment_matrix[1][1], moment_matrix[2][0]]]
     covariance_matrix /= (zeroth_moment)
     # the volume under multivariate is 10,000. Thus, we have to divide with 10000 to get a right value
@@ -67,7 +73,7 @@ def null_deblend_plot(flux, first_moment_x, first_moment_y, covariance_matrix):
     """
     x, y = np.mgrid[x_min:x_max:distance, y_min:y_max:distance]
     pos = np.dstack((x, y))
-    rv = scipy.stats.multivariate_normal([first_moment_x,first_moment_y], covariance_matrix, allow_singular=True) #FIX BUG     
+    rv = scipy.stats.multivariate_normal([first_moment_x,first_moment_y], covariance_matrix, allow_singular=True) #FIX BUG
     image = [[0]*number_of_rows for _ in range(number_of_columns)]
     image = image + rv.pdf(pos)*flux
     return image
